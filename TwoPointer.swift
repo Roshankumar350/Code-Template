7de6f583eba27aa8697e16eb6bb6d6a621d01fdd
Roshan Kumar Sah <roshankumar350@gmail.com>
2025-10-05 16:01:59 +0530

    Two pointers: two inputs, exhaust both
    Two pointers: two inputs, exhaust both

--- TwoPointer.swift
+++ TwoPointer.swift
@@ -18,3 +18,27 @@ func twoPointer(_ inputArray: [Int]) {
         lastIndex -= 1 
     }
 }
+
+// Two pointers: two inputs, exhaust both
+func twoPointer(_ input1Array: [Int], _ input2Array: [Int]) {
+    let input1ArraySize = input1Array.count
+    var input1ArrayLeftElement = 0
+    let input2ArraySize = input2Array.count
+    var input2ArrayLeftElement = 0
+    
+    while (input1ArrayLeftElement < input1ArraySize) && (input2ArrayLeftElement < input2ArraySize) { 
+        print("input1Array input2Array", input1Array[input1ArrayLeftElement], input2Array[input2ArrayLeftElement])
+        input1ArrayLeftElement += 1
+        input2ArrayLeftElement += 1
+    }
+    
+    while (input1ArrayLeftElement < input1ArraySize) {
+        print("input1Array", input1Array[input1ArrayLeftElement])
+        input1ArrayLeftElement += 1
+    }
+    
+    while (input2ArrayLeftElement < input2ArraySize) {
+        print("input2Array", input2Array[input2ArrayLeftElement])
+        input2ArrayLeftElement += 1
+    }
+}
